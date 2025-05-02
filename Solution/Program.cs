5c8d414aebbb393436a71240ffa7b08ad265c0c4
Jiowolf <msel2lachts@gmail.com>
2025-05-02 16:06:59 +0200

    begin display and add , test obj?

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,15 +1,46 @@
 using System;
+using System.IO;
 
+
+public class Vcard
+{
+    string firstName = "first name";
+    string lastName = "last name";
+
+}
 public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        
     }
 
-    public static string DisplayCards()
+    public static void DisplayAllCards()
     {
+        string line;
+        try
+        {
+            StreamReader sr = new StreamReader("contacts.vcf");
+            line = sr.ReadLine();
 
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            Console.ReadLine();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("program Stop");
+        }
+
+        
     }
 
     public static void SearchCards()
@@ -20,6 +51,8 @@ public class Program
     public static string AddCards(string firstName,string lastName, int tel, string email)
     {
         string vCard = $"BEGIN:VCARD\nFN:{firstName} {lastName}\nTEL:{tel}\nEMAIL:{email}\nEND:VCARD\n\n";
+
+        return vCard;
     }
 
     public static void DeleteCards()
