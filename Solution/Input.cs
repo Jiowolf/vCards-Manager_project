1af285509996a2bba72db06b9389c1dc1723d093
Jiowolf <msel2lachts@gmail.com>
2025-05-26 14:55:40 +0200

    Add card ok but List to do

---
+++ Solution/Input.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    public class Input
+    {
+        public int InputTel()
+        {
+            Regex regexTel = new Regex(@"^\+\d{1,3}\s\d{2,4}/\d{2}\.\d{2}\.\d{2}$");
+
+            while (true)
+            {
+                Console.WriteLine("Input a telephone number with the european format:");
+                Console.WriteLine("+## ###/##.##.## OU +### ###/##.##.##");
+
+                string input = Console.ReadLine();
+
+                if (regexTel.IsMatch(input))
+                {
+                    int intPut = int.Parse(input);
+
+                    return intPut;
+                }
+                else
+                {
+                    Console.WriteLine("Format invalide\n");
+                }
+            }
+        }
+    }
+}
--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,51 +1,117 @@
 using System;
 using System.IO;
+using Solution;
 
 
 public class Vcard
 {
-    string firstName = "first name";
-    string lastName = "last name";
+    string? FirstName { get; set; }
+    string? LastName { get; set; }
+    int Telephone {  get; set; }
+    string? Email { get; set; }
 
 }
 public class Program
 {
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("");
+
+        Console.WriteLine("Hello There");
+
+        char answer = '?';
+        while (answer != '6')
+        {
+            Console.WriteLine("\n\nChoose a option:\n");
+
+            Console.WriteLine("1: Display all contatcs");
+            Console.WriteLine("2: Add a new contacts");
+            Console.WriteLine("3: Search for a contact by name");
+            Console.WriteLine("4: Delete a conbtact");
+            Console.WriteLine("5: Export a contact into a separate file");
+            Console.WriteLine("6: End program\n");
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            answer = keyInfo.KeyChar;
+
+            switch (answer)
+            {
+                default:
+                    Console.WriteLine("\n\nInvalide option, try again!");
+                    break;
+
+                case '1':
+                    DisplayAllCards();
+                    break;
+
+                case '2':
+                    var input = new Input();
+
+                    string firstName = Console.ReadLine();
+                    string lastName = Console.ReadLine();
+                    int tel = input.InputTel();
+                    string email = Console.ReadLine();
+
+                    string addUp = AddCards(firstName,lastName,tel,email);
+
+                    //
+
+                    break;
+
+                case '3':
+
+                    break;
+
+                case '4':
+
+                    break;
+
+                case '5':
+
+                    break;
+
+                case '6':
+                    Console.WriteLine("\nThx for using vCard manager 2000 ;)");
+                    break;
+            }
+
+
+        }
+
     }
 
     public static void DisplayAllCards()
     {
-        string line;
+        string line = "";
         try
         {
-            StreamReader sr = new StreamReader("contacts.vcf");
-            line = sr.ReadLine();
+            string filePath = Path.Combine(AppContext.BaseDirectory, "contacts.vcf");
+            StreamReader sr = new StreamReader(filePath);
+
+            //line = sr.ReadLine();
 
             while (line != null)
             {
                 Console.WriteLine(line);
                 line = sr.ReadLine();
             }
+            if (line == null)
+            {
+                Console.WriteLine("Press Enter to confirm");
+            }
             sr.Close();
             Console.ReadLine();
         }
-        catch (Exception e)
+        catch (IOException e)
         {
             Console.WriteLine("Exception: " + e.Message);
         }
         finally
         {
-            Console.WriteLine("program Stop");
+            Console.WriteLine("Finish displaying");
         }
 
         
-    }
-
-    public static void SearchCards()
-    {
-
     }
 
     public static string AddCards(string firstName,string lastName, int tel, string email)
@@ -53,6 +119,11 @@ public class Program
         string vCard = $"BEGIN:VCARD\nFN:{firstName} {lastName}\nTEL:{tel}\nEMAIL:{email}\nEND:VCARD\n\n";
 
         return vCard;
+    }
+
+    public static void SearchCards()
+    {
+
     }
 
     public static void DeleteCards()
